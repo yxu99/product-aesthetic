b5da5d25b71e080dabf6d54b5cd05d4edb332a10
jjasonbell <j.jason.bell@gmail.com>
2018-08-28 17:53:07 +0100

    Add comparison of means.

--- Jason/bayesian_reg.R
+++ Jason/bayesian_reg.R
@@ -21,20 +21,39 @@ X_ac <- as.matrix(model.matrix(aes_eval ~ product -1, data=aes_ac))
 N_ac <- nrow(X_ac)
 K_ac <- ncol(X_ac)
 stan_inputs_ac <- list(X=X_ac, N=N_ac, y=aes_ac$aes_eval, K=K_ac)
-ac_fit <- stan(file='regression.stan', data=stan_inputs_ac, cores=2, chains=2, iter=2000)
+R <- 2000
+ac_fit <- stan(file='regression.stan', data=stan_inputs_ac, cores=2, chains=2, iter=R)
 draws_ac <- extract(ac_fit)
 #launch_shinystan(ac_fit)
 X_afh <- as.matrix(model.matrix(aes_eval ~ product -1, data=aes_afh))
 N_afh <- nrow(X_afh)
 K_afh <- ncol(X_afh)
 stan_inputs_afh <- list(X=X_afh, N=N_afh, y=aes_afh$aes_eval, K=K_afh)
-afh_fit <- stan(file='regression.stan', data=stan_inputs_afh, cores=2, chains=2, iter=2000)
+afh_fit <- stan(file='regression.stan', data=stan_inputs_afh, cores=2, chains=2, iter=R)
 draws_afh <- extract(afh_fit)
+# Compare sigmas
 sigs_ac <- draws_ac$sigma
 sigs_afh <- draws_afh$sigma
 sig_diff_a <- sigs_ac - sigs_afh
-cat("95% Posterior Density Interval:", quantile(sig_diff_a, 0.025), ", ", quantile(sig_diff_a, 0.975))
+cat("95% Posterior Density Interval for difference in s.d.:", 
+    quantile(sig_diff_a, 0.025), ", ", quantile(sig_diff_a, 0.975))
+# Compare means (mu = X %*% beta)
+mu_ac <- rep(NA, R)
+mu_afh <- rep(NA, R)
+for (r in (R / 2):R) {
+    mu_ac[r] <- mean(X_ac %*% draws_ac$beta[r, ])
+    mu_afh[r] <- mean(X_afh %*% draws_afh$beta[r, ])
+}
+mu_ac <- mu_ac[!is.na(mu_ac)]
+mu_afh <- mu_afh[!is.na(mu_afh)]
+mu_diff_a <- mu_ac - mu_afh
+cat("95% Posterior Density Interval for difference in means:", 
+    quantile(mu_diff_a, 0.025), ", ", quantile(mu_diff_a, 0.975))
+# Significantly different mean: much lower in control condition
 
+
+
+# For impact of aesthetic ratings on functional distribution
 fun_compare <- aes_data[aes_data$Version == "FC" | aes_data$Version == "FAH", 
                         c(grep(glob2rx("*_FEV"), names(aes_data)), 
                           grep("Version", names(aes_data)))]
@@ -49,16 +68,31 @@ X_fc <- as.matrix(model.matrix(fun_eval ~ product -1, data=fun_fc))
 N_fc <- nrow(X_fc)
 K_fc <- ncol(X_fc)
 stan_inputs_fc <- list(X=X_fc, N=N_fc, y=fun_fc$fun_eval, K=K_fc)
-fc_fit <- stan(file='regression.stan', data=stan_inputs_fc, cores=2, chains=2, iter=2000)
+fc_fit <- stan(file='regression.stan', data=stan_inputs_fc, cores=2, chains=2, iter=R)
 draws_fc <- extract(fc_fit)
 #launch_shinystan(fc_fit)
 X_fah <- as.matrix(model.matrix(fun_eval ~ product -1, data=fun_fah))
 N_fah <- nrow(X_fah)
 K_fah <- ncol(X_fah)
 stan_inputs_fah <- list(X=X_fah, N=N_fah, y=fun_fah$fun_eval, K=K_fah)
-fah_fit <- stan(file='regression.stan', data=stan_inputs_fah, cores=2, chains=2, iter=2000)
+fah_fit <- stan(file='regression.stan', data=stan_inputs_fah, cores=2, chains=2, iter=R)
 draws_fah <- extract(fah_fit)
-sigs_fc <- draws_fc$sigma
-sigs_fah <- draws_fah$sigma
-sig_diff_a <- sigs_fc - sigs_fah
-cat("95% Posterior Density Interval:", quantile(sig_diff_a, 0.025), ", ", quantile(sig_diff_a, 0.975))
+sigs_fc <- draws_fc$sigma[(R / 2):R]
+sigs_fah <- draws_fah$sigma[(R / 2):R]
+sig_diff_f <- sigs_fc - sigs_fah
+cat("95% Posterior Density Interval for difference in s.d.:", 
+    quantile(sig_diff_f, 0.025), ", ", quantile(sig_diff_f, 0.975))
+# Compare means (mu = X %*% beta)
+mu_fc <- rep(NA, R)
+mu_fah <- rep(NA, R)
+for (r in (R / 2):R) {
+    mu_fc[r] <- mean(X_fc %*% draws_fc$beta[r, ])
+    mu_fah[r] <- mean(X_fah %*% draws_fah$beta[r, ])
+}
+mu_fc <- mu_fc[!is.na(mu_fc)]
+mu_fah <- mu_fah[!is.na(mu_fah)]
+mu_diff_f <- mu_fc - mu_fah
+cat("95% Posterior Density Interval for difference in means:", 
+    quantile(mu_diff_f, 0.025), ", ", quantile(mu_diff_f, 0.975))
+# Significantly different mean: much lower in control condition
+
